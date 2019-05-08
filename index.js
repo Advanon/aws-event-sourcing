9fa61d2625412c3220bcb7c7b4c51532495d039b
Ross McEwan <rossmcewan@gmail.com>
2019-05-08 23:27:40 +0200

    fix index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Aggregate = require('./src/lib/Aggregate');
-const AggregateOptions = require('./src/lib/AggregateOptions');
 const Event = require('./src/lib/Event');
 const IllegalEventError = require('./src/lib/IllegalEventError');
 const IllegalEventNumberError = require('./src/lib/IllegalEventNumberError');
@@ -8,7 +7,6 @@ const Resource = require('./src/lib/Resource');
 
 module.exports = {
     Aggregate,
-    AggregateOptions,
     Event,
     IllegalEventError,
     IllegalEventNumberError,

336ae4336132cb4679d388eb82cd65e1cdf0919d
Andrea Pivetta <andrea.pivetta@advanon.com>
2019-10-16 16:41:00 +0200

    Refactor repository

--- src/lib/Repository.js
+++ src/lib/Repository.js
@@ -1,46 +1,46 @@
 const { dynamoDb } = require("aws-access-utils");
 
-module.exports = {
-  query: async (table, id) => {
-    const { Items } = await dynamoDb.doc
-      .query({
-        TableName: table,
-        KeyConditionExpression: "id = :id",
-        ScanIndexForward: true,
-        ExpressionAttributeValues: {
-          ":id": id
-        }
-      })
-      .promise();
-    return Items;
-  },
-
-  create: async (table, id, event) => {
-    await dynamoDb.doc
-      .put({
-        TableName: table,
-        ConditionExpression: "attribute_not_exists(id)",
-        Item: {
-          id: id,
-          ...event
-        }
-      })
-      .promise();
-  },
-
-  add: async (table, id, event) => {
-    await dynamoDb.doc
-      .put({
-        TableName: table,
-        ConditionExpression: "attribute_not_exists(#eventNumber)",
-        ExpressionAttributeNames: {
-          "#eventNumber": "number"
-        },
-        Item: {
-          id: id,
-          ...event
-        }
-      })
-      .promise();
-  }
+const query = async (TableName, id) => {
+  const { Items } = await dynamoDb.doc
+    .query({
+      TableName,
+      KeyConditionExpression: "id = :id",
+      ScanIndexForward: true,
+      ExpressionAttributeValues: {
+        ":id": id
+      }
+    })
+    .promise();
+  return Items;
 };
+
+const create = async (TableName, id, event) => {
+  await dynamoDb.doc
+    .put({
+      TableName,
+      ConditionExpression: "attribute_not_exists(id)",
+      Item: {
+        id: id,
+        ...event
+      }
+    })
+    .promise();
+};
+
+const add = async (TableName, id, event) => {
+  await dynamoDb.doc
+    .put({
+      TableName,
+      ConditionExpression: "attribute_not_exists(#eventNumber)",
+      ExpressionAttributeNames: {
+        "#eventNumber": "number"
+      },
+      Item: {
+        id: id,
+        ...event
+      }
+    })
+    .promise();
+};
+
+module.exports = { query, create, add };
